b1d887de9dd7d8207d660f81d18889f7ac78545a
Tyler <tylerturek5804@gmail.com>
2024-07-10 14:36:46 -0400

    Modifying jump threshold to make more accurate real data calculations

--- Analyzing_Jumps_Real_Data.py
+++ Analyzing_Jumps_Real_Data.py
@@ -19,13 +19,15 @@ import time
 # Parameters for analyzing the jump
 jump_threshold = 0.005
 alpha_threshold = 0.75
+old_alpha_thresold = 0.1
 sqrt_synchronous = 5
-seperation = 10
+seperation = 25
+smoothing_index = 100
 
 # Paraemeters for the data to look at
 start_index = 0
 total_number = 10000
-detector_number = 100
+detector_number = 0
 
 # Parameters for number of scales for the wavelet transform
 number_scales = 3
@@ -41,25 +43,46 @@ fig3, axs3 = plt.subplots(sqrt_synchronous, sqrt_synchronous, figsize=(15, 15))
 fig4, axs4 = plt.subplots(sqrt_synchronous, sqrt_synchronous, figsize=(15, 15))
 
 # Time axis
-time_axis = np.linspace(0, 1, total_number, endpoint=False)
+time_axis = np.linspace(0, 10000, total_number, endpoint=False)
+time_axis2 = np.linspace(0, 10000, total_number - smoothing_index + 1, endpoint=False)
 
 for i in range(sqrt_synchronous):
     for j in range(sqrt_synchronous):
         # Setting up the output print
-        title_text = f"For Detector Number {(detector_number + i * sqrt_synchronous + j) * seperation}"
+        title_text = f"For Detector Number {detector_number + (i * sqrt_synchronous + j) * seperation}"
         print("-------------------------------------------------------------")
         Singularity_Analysis.print_colored(title_text, "blue")
 
         # Getting the real data
-        original_data = Generating_Real_Data.get_real_data(start_index, total_number, (detector_number + i * sqrt_synchronous + j) * seperation)
+        original_data = Generating_Real_Data.get_real_data(start_index, total_number, detector_number + (i * sqrt_synchronous + j) * seperation)
+        
+        # Getting the power of noise and SNR for the signal itself
+        smooth_data = np.convolve(original_data, np.ones(smoothing_index)/smoothing_index, mode='valid')
+        deviation_data = original_data[0 : total_number - smoothing_index + 1] - smooth_data
+        noise_power = np.mean(deviation_data**2)
+        signal_power = np.mean(original_data**2)
+        signal_noise_ratio = signal_power / noise_power
 
         # Running the wavelet transform
         wavelet_transform, time_series = MZ_Wavelet_Transforms.forward_wavelet_transform(number_scales, original_data)
         processed_data = MZ_Wavelet_Transforms.inverse_wavelet_transform(wavelet_transform, time_series)
+        
+        # Getting the power of noise and SNR for the wavelet transform 
+        first_wavelet_transform = wavelet_transform[:, 0]
+        smooth_wavelet = np.convolve(first_wavelet_transform, np.ones(smoothing_index)/smoothing_index, mode='valid')
+        deviation_wavelet = first_wavelet_transform[0 : total_number - smoothing_index + 1] - smooth_wavelet
+        noise_power_wavelet = np.mean(deviation_wavelet**2)
+        signal_power_wavelet = np.mean(first_wavelet_transform**2)
+        signal_noise_ratio_wavelet = signal_power_wavelet / noise_power_wavelet
+
+        # Modifying jump threshold based on standard deviation of the wavelet transform
+        standard_deviation = np.std(wavelet_transform)
+        jump_threshold = 5 * standard_deviation
 
         # Plotting the original signal
         axs1[i, j].plot(time_axis, original_data)
-        axs1[i, j].set_title(f"Original Signal of Detector {(detector_number + i * sqrt_synchronous + j) * seperation}", fontsize = 8)
+        axs1[i, j].plot(time_axis2, smooth_data)
+        axs1[i, j].set_title(f"Original Signal of Detector {detector_number + (i * sqrt_synchronous + j) * seperation}", fontsize = 8)
         axs1[i, j].set_xlabel("Time (s)", fontsize = 6)
         axs1[i, j].set_ylabel("Amplitude", fontsize = 6)
         axs1[i, j].tick_params(axis = "both", labelsize = 4)
@@ -67,7 +90,8 @@ for i in range(sqrt_synchronous):
 
         # Plotting the wavelet transform
         axs2[i, j].plot(time_axis, wavelet_transform)
-        axs2[i, j].set_title(f"Wavlet Transform of Detector {(detector_number + i * sqrt_synchronous + j) * seperation}", fontsize = 8)
+        axs2[i, j].plot(time_axis2, smooth_wavelet)
+        axs2[i, j].set_title(f"Wavlet Transform of Detector {detector_number + (i * sqrt_synchronous + j) * seperation}", fontsize = 8)
         axs2[i, j].set_xlabel("Time (s)", fontsize = 6)
         axs2[i, j].set_ylabel("Value", fontsize = 6)
         axs2[i, j].tick_params(axis = "both", labelsize = 4)
@@ -88,11 +112,11 @@ for i in range(sqrt_synchronous):
         # Looking for jumps based on the old alpha values
         # 
         old_alpha_values = Singularity_Analysis.compute_alpha_values(wavelet_transform)
-        old_alpha_jump_indexes = Singularity_Analysis.compute_alpha_indexes(old_alpha_values, alpha_threshold)
+        old_alpha_jump_indexes = Singularity_Analysis.compute_alpha_indexes(old_alpha_values, old_alpha_thresold)
 
         # Plotting the alpha values
         axs3[i, j].plot(time_axis, old_alpha_values)
-        axs3[i, j].set_title(f"Alpha from Old Method of Detector {(detector_number + i * sqrt_synchronous + j) * seperation}", fontsize = 8)
+        axs3[i, j].set_title(f"Alpha from Old Method of Detector {detector_number + (i * sqrt_synchronous + j) * seperation}", fontsize = 8)
         axs3[i, j].set_xlabel("Time (s)", fontsize = 6)
         axs3[i, j].set_ylabel("Alpha", fontsize = 6)
         axs3[i, j].tick_params(axis = "both", labelsize = 4)
@@ -107,11 +131,13 @@ for i in range(sqrt_synchronous):
         alpha_values, alpha_jump_indexes = Singularity_Analysis.packaged_compute_alpha_values_and_indexes(wavelet_transform, 1, jump_threshold, alpha_threshold)
 
         # Plotting the new alpha values
-        axs4[i, j].plot(time_axis, alpha_values)
-        axs4[i, j].set_title(f"Alpha from New Method of Detector {(detector_number + i * sqrt_synchronous + j) * seperation}", fontsize = 8)
+        axs4[i, j].plot(time_axis, alpha_values, marker = ".")
+        axs4[i, j].set_title(f"Alpha from New Method of Detector {detector_number + (i * sqrt_synchronous + j) * seperation}", fontsize = 8)
         axs4[i, j].set_xlabel("Time (s)", fontsize = 6)
         axs4[i, j].set_ylabel("Alpha", fontsize = 6)
         axs4[i, j].tick_params(axis = "both", labelsize = 4)
+        axs4[i, j].axhline(y = alpha_threshold, color = "r", linestyle = "--")
+        axs4[i, j].axhline(y = -1 * alpha_threshold, color = "r", linestyle = "--")
         axs4[i, j].grid(True)
 
         new_alpha_text = f"Indexes Where Jumps are Suspected based on New Alpha ({alpha_jump_indexes.size} total Jumps): {alpha_jump_indexes}"
@@ -135,135 +161,3 @@ fig1.savefig("Signals.png")
 fig2.savefig("Transforms.png")
 fig3.savefig("Old_Alphas.png")
 fig4.savefig("New_Alphas.png")
-
-
-
-
-
-
-
-
-
-
-# # -------------------------------
-# # Getting the data
-# # -------------------------------
-
-# # Getting the real data
-# original_data = Generating_Real_Data.get_real_data(start_index, total_number, detector_number)
-
-# # Creating the time axis
-# time_axis = np.linspace(0, 1, total_number, endpoint=False)
-
-# # -------------------------------
-# # Running the wavelet transform
-# # -------------------------------
-
-# wavelet_transform, time_series = MZ_Wavelet_Transforms.forward_wavelet_transform(number_scales, original_data)
-# processed_data = MZ_Wavelet_Transforms.inverse_wavelet_transform(wavelet_transform, time_series)
-
-# # -------------------------------
-# # Generating Plots
-# # -------------------------------
-
-# # Plotting the original signal
-# plt.figure(1)
-# plt.plot(time_axis, original_data)
-# plt.xlabel("Time (s)", fontsize = 6)
-# plt.ylabel("Amplitude", fontsize = 6)
-# plt.title(f"Original Signal of Detector {detector_number}", fontsize = 8)
-# plt.grid(True)
-# plt.savefig("Original_Signal.png")
-
-# # Plotting the wavelet transform
-# plt.figure(2)
-# plt.plot(time_axis, wavelet_transform)
-# plt.xlabel("Time (s)", fontsize = 6)
-# plt.ylabel("Value", fontsize = 6)
-# plt.title(f"Wavlet Transform of Detector {detector_number}", fontsize = 8)
-# plt.grid(True)
-# plt.savefig("Wavelet_Transform.png")
-
-# # -------------------------------
-# # Looking for jumps based on behavior
-# # -------------------------------
-
-# # Using the compute_jump_locations function to find jumps
-# jump_indexes = Singularity_Analysis.compute_jump_locations(wavelet_transform, jump_threshold)
-
-# # Printing the results
-# print("-------------------------------------------------------------")
-# Singularity_Analysis.print_colored("Indexes Where Jumps are Suspected based on Behavior", "magenta")
-# print(jump_indexes)
-# print("Number of jumps", jump_indexes.size)
-# print("-------------------------------------------------------------")
-# print("")
-
-# # -------------------------------
-# # Looking for jumps based on the old alpha values
-# # -------------------------------
-
-# # Using the compute_alpha_values function to calcualte alpha for entire data set
-# old_alpha_values = Singularity_Analysis.compute_alpha_values(wavelet_transform)
-
-# # Getting the indexes
-# old_alpha_jump_indexes = Singularity_Analysis.compute_alpha_indexes(old_alpha_values, alpha_threshold)
-
-# # Plotting the alpha values
-# plt.figure(3)
-# plt.axhline(y = 0, color = "r")
-# plt.plot(time_axis, old_alpha_values)
-# plt.xlabel("Time (s)", fontsize = 6)
-# plt.ylabel("Alpha", fontsize = 6)
-# plt.title(f"Alpha Values as Calculated via the Old Method of Detector {detector_number}", fontsize = 8)
-# plt.grid(True)
-# plt.savefig("Old_Alpha.png")
-
-# print("-------------------------------------------------------------")
-# Singularity_Analysis.print_colored("Indexes Where Jumps are Suspected based on Old Alpha", "magenta")
-# print(old_alpha_jump_indexes)
-# print("Number of jumps", old_alpha_jump_indexes.size)
-# print("-------------------------------------------------------------")
-# print("")
-
-# # -------------------------------
-# # Looking for jumps based on the new alpha values
-# # -------------------------------
-
-# # Using the function to calcualte alpha for entire data set
-# alpha_values, alpha_jump_indexes = Singularity_Analysis.packaged_compute_alpha_values_and_indexes(wavelet_transform, 1, jump_threshold, alpha_threshold)
-
-# # Plotting the new alpha values
-# plt.figure(4)
-# plt.axhline(y = 0, color = "r")
-# plt.plot(time_axis, alpha_values)
-# plt.xlabel("Time (s)", fontsize = 6)
-# plt.ylabel("Alpha", fontsize = 6)
-# plt.title(f"Alpha Values as Calculated via the New Method of Detector {detector_number}", fontsize = 8)
-# plt.grid(True)
-# plt.savefig("New_Alpha.png")
-
-
-# print("-------------------------------------------------------------")
-# Singularity_Analysis.print_colored("Indexes Where Jumps are Suspected based on New Alpha", "magenta")
-# print(alpha_jump_indexes)
-# print("Number of jumps", alpha_jump_indexes.size)
-# print("-------------------------------------------------------------")
-# print("")
-
-# # -------------------------------
-# # Looking at ways to better compute alphas that matter
-# # -------------------------------
-
-# #
-# # Method 0: Possible jumps based on both behavior and alpha
-# #
-
-# behavior_jumps_indexes = np.array(jump_indexes)
-# combined = np.intersect1d(alpha_jump_indexes, jump_indexes)
-
-# print("-------------------------------------------------------------")
-# print("Indexes Where Jumps are Suspected based on Beahvior AND Alpha")
-# Singularity_Analysis.print_colored(combined, "cyan")
-# print("-------------------------------------------------------------")
-# print("")
--- Generating_Real_Data.py
+++ Generating_Real_Data.py
@@ -8,6 +8,16 @@ from sotodlib.core import AxisManager
 # ()()()()()()()()()()()()()
 
 # -------------------------------
+# Extracting the raw data to improve compute time
+# -------------------------------
+
+# Loading in the raw real data
+global_raw_data = AxisManager.load("/mnt/welch/SO/obs_1704900313_lati1_111.h5")
+
+# Focusing only on detectors that are set up properly
+global_raw_data.restrict("dets", global_raw_data.det_cal.bg > 0)
+
+# -------------------------------
 # get_real_data
 # Generates real data for further analysis
 # start_index     : integer
@@ -21,18 +31,31 @@ from sotodlib.core import AxisManager
 # -------------------------------
 
 def get_real_data(start_index, length, detector_number):
-    # Loading in the raw real data
-    raw_data = AxisManager.load("/mnt/welch/SO/obs_1704900313_lati1_111.h5")
+    current_raw_data = global_raw_data
 
     # Only focusing on the first "length" samples for every detector
-    raw_data.restrict("samps", slice(start_index, start_index + length))
-
-    # Focusing only on detectors that are set up properly
-    raw_data.restrict("dets", raw_data.det_cal.bg > 0)
+    current_raw_data.restrict("samps", slice(start_index, start_index + length))
 
     # Further processing the data
-    raw_data.preprocess
-    raw_data.preprocess.noise
-    raw_data.preprocess.noise.white_noise
+    current_raw_data.preprocess
+    current_raw_data.preprocess.noise
+    current_raw_data.preprocess.noise.white_noise
 
-    return raw_data.signal[detector_number]
+    return current_raw_data.signal[detector_number]
+
+# def get_real_data(start_index, length, detector_number):
+#     # Loading in the raw real data
+#     raw_data = AxisManager.load("/mnt/welch/SO/obs_1704900313_lati1_111.h5")
+
+#     # Only focusing on the first "length" samples for every detector
+#     raw_data.restrict("samps", slice(start_index, start_index + length))
+
+#     # Focusing only on detectors that are set up properly
+#     raw_data.restrict("dets", raw_data.det_cal.bg > 0)
+
+#     # Further processing the data
+#     raw_data.preprocess
+#     raw_data.preprocess.noise
+#     raw_data.preprocess.noise.white_noise
+
+#     return raw_data.signal[detector_number]
