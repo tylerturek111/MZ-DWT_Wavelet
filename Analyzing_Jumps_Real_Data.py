0848aaeeb282fd38a63365708f749f4347c64fe0
Tyler <tylerturek5804@gmail.com>
2024-07-12 16:46:44 -0400

    Further refined plot generation and started working with noise

--- Analyzing_Jumps_Real_Data.py
+++ Analyzing_Jumps_Real_Data.py
@@ -6,7 +6,7 @@ import pywt
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-import time
+import os
 
 # ()()()()()()()()()()()()()
 # This file utilizes the functions in Singularity_Analysis to look at "jumps" with real data
@@ -17,24 +17,28 @@ import time
 # -------------------------------
 
 # Parameters for analyzing the jump
-jump_threshold = 0.005
+sd_ratio = 5
 alpha_threshold = 0.75
-old_alpha_thresold = 0.1
-sqrt_synchronous = 5
-seperation = 1
-smoothing_index = 100
-window_size = 10
 
-# Paraemeters for the data to look at
+# Parameters for the detectors to look at
+detector_number = 0
+seperation = 1
+sqrt_synchronous = 2
+
+# Paraemeters for the time data to look at
 start_index = 0
 total_number = 10000
-detector_number = 0
 
 # Parameters for number of scales for the wavelet transform
 number_scales = 3
 
+# Other parameters
+old_alpha_thresold = 0.1
+smoothing_index = 100
+window_size = 100
+
 # -------------------------------
-# Looking at multiple detectprs at once
+# Setting up stoff
 # -------------------------------
 
 # Setting up the graphs
@@ -43,10 +47,22 @@ fig2, axs2 = plt.subplots(sqrt_synchronous, sqrt_synchronous, figsize=(15, 15))
 fig3, axs3 = plt.subplots(sqrt_synchronous, sqrt_synchronous, figsize=(15, 15))
 fig4, axs4 = plt.subplots(sqrt_synchronous, sqrt_synchronous, figsize=(15, 15))
 
-# Time axis
+# Setting up the time axis
 time_axis = np.linspace(0, 10000, total_number, endpoint=False)
 time_axis2 = np.linspace(0, 10000, total_number - smoothing_index + 1, endpoint=False)
 summary_stats = []
+
+# Setting up the path to store the figures
+folder = "../Jump_Plots"
+if not os.path.exists(folder):
+    os.makedirs(folder)
+folder2 = "../Summary_Plots"
+if not os.path.exists(folder2):
+    os.makedirs(folder2)
+
+# -------------------------------
+# Looking at multiple detectprs at once
+# -------------------------------
 
 for i in range(sqrt_synchronous):
     for j in range(sqrt_synchronous):
@@ -59,14 +75,20 @@ for i in range(sqrt_synchronous):
         Singularity_Analysis.print_colored(title_text, "blue")
 
         # Getting the real data
-        original_data = Generating_Real_Data.get_real_data(start_index, total_number, current_detector)
-        
+        original_data, noise_level = Generating_Real_Data.get_real_data(start_index, total_number, current_detector)
+
+        print(f"Noise Level for {i}, {j}: {noise_level}")
+
         # Getting the power of noise and SNR for the signal itself
         smooth_data = np.convolve(original_data, np.ones(smoothing_index)/smoothing_index, mode='valid')
         deviation_data = original_data[0 : total_number - smoothing_index + 1] - smooth_data
         noise_power = np.mean(deviation_data**2)
         signal_power = np.mean(original_data**2)
         signal_noise_ratio = signal_power / noise_power
+        signal_noise_ratio_plus = 10 * math.log(signal_power / noise_power)
+
+        print(f"Signal Power {signal_power}")
+        print(f"Noise Power {noise_power}")
 
         # Running the wavelet transform
         wavelet_transform, time_series = MZ_Wavelet_Transforms.forward_wavelet_transform(number_scales, original_data)
@@ -82,11 +104,19 @@ for i in range(sqrt_synchronous):
 
         # Modifying jump threshold based on standard deviation of the wavelet transform
         standard_deviation = np.std(wavelet_transform)
-        jump_threshold = 4.5 * standard_deviation
+        jump_threshold = sd_ratio * standard_deviation
+
+        print(f"Standard Deviation for {i}, {j}: {standard_deviation}")
+
+        print(f"Ratio for {i}, {j}: {standard_deviation / noise_level}")
+
+        print(f"SNR {signal_noise_ratio}")
+        
+        print(f"SNR Plus {signal_noise_ratio_plus}")
 
         # Plotting the original signal
         axs1[i, j].plot(time_axis, original_data)
-        axs1[i, j].plot(time_axis2, smooth_data)
+        # axs1[i, j].plot(time_axis2, smooth_data)
         axs1[i, j].set_title(f"Original Signal of Detector {current_detector}", fontsize = 8)
         axs1[i, j].set_xlabel("Time (s)", fontsize = 6)
         axs1[i, j].set_ylabel("Amplitude", fontsize = 6)
@@ -158,7 +188,6 @@ for i in range(sqrt_synchronous):
         combined_text = f"Indexes Where Jumps are Suspected based on Beahvior AND Alpha ({combined.size} total Jumps): {combined}"
         Singularity_Analysis.print_colored(combined_text, "cyan")
 
-
         #
         # Saving the possible jumps all together and generating plots of their locations
         # 
@@ -183,8 +212,9 @@ for i in range(sqrt_synchronous):
                 ax5.axvline(x = combined[j] + 1, color = "r", linestyle = "--")
 
                 # Saving the plot
-                figure_name = f"{current_detector}_{combined[j]}"
-                fig5.savefig(figure_name)
+                file_name = f"{current_detector}_{combined[j]}"
+                path = os.path.join(folder, file_name)
+                fig5.savefig(path)
 
         # Ending the printing output for each detector
         print("-------------------------------------------------------------")
@@ -199,7 +229,12 @@ for i in range(summary_stats.shape[0]):
     Singularity_Analysis.print_colored(current_text, "green")
 Singularity_Analysis.print_colored("(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(", "red")
 
-fig1.savefig("Signals.png")
-fig2.savefig("Transforms.png")
-fig3.savefig("Old_Alphas.png")
-fig4.savefig("New_Alphas.png")
+# Saving the summary plots
+path1 = os.path.join(folder2, "Signals")
+path2 = os.path.join(folder2, "Transforms")
+path3 = os.path.join(folder2, "Old_Alpha")
+path4 = os.path.join(folder2, "New_Alpha")
+fig1.savefig(path1)
+fig2.savefig(path2)
+fig3.savefig(path3)
+fig4.savefig(path4)
--- Generating_Real_Data.py
+++ Generating_Real_Data.py
@@ -17,17 +17,22 @@ global_raw_data = AxisManager.load("/mnt/welch/SO/obs_1704900313_lati1_111.h5")
 # Focusing only on detectors that are set up properly
 global_raw_data.restrict("dets", global_raw_data.det_cal.bg >= 0)
 
+# Focusing only on detectors that have accompanying background data
+global_raw_data.restrict("dets", global_raw_data.preprocess.noise.white_noise > 0)
+
 # -------------------------------
 # get_real_data
 # Generates real data for further analysis
-# start_index     : integer
-#                   The point at which jump analysis start
-# length          : integer
-#                   The number of samples to be analyzed
-# detector_number : integer
-#                   The detector number to be analyzed
-# Returns         : 1-D Numpy Array of floats
-#                   The modified actual data to look at
+# start_index      : integer
+#                    The point at which jump analysis start
+# length           : integer
+#                    The number of samples to be analyzed
+# detector_number  : integer
+#                    The detector number to be analyzed
+# RETURNS, returns : 1-D Numpy Array of floats
+#                    The modified actual data to look at
+# returns, RETURNS : integer
+#                    The noise value of a given detector
 # -------------------------------
 
 def get_real_data(start_index, length, detector_number):
@@ -36,26 +41,4 @@ def get_real_data(start_index, length, detector_number):
     # Only focusing on the first "length" samples for every detector
     current_raw_data.restrict("samps", slice(start_index, start_index + length))
 
-    # Further processing the data
-    current_raw_data.preprocess
-    current_raw_data.preprocess.noise
-    current_raw_data.preprocess.noise.white_noise
-
-    return current_raw_data.signal[detector_number]
-
-# def get_real_data(start_index, length, detector_number):
-#     # Loading in the raw real data
-#     raw_data = AxisManager.load("/mnt/welch/SO/obs_1704900313_lati1_111.h5")
-
-#     # Only focusing on the first "length" samples for every detector
-#     raw_data.restrict("samps", slice(start_index, start_index + length))
-
-#     # Focusing only on detectors that are set up properly
-#     raw_data.restrict("dets", raw_data.det_cal.bg > 0)
-
-#     # Further processing the data
-#     raw_data.preprocess
-#     raw_data.preprocess.noise
-#     raw_data.preprocess.noise.white_noise
-
-#     return raw_data.signal[detector_number]
+    return current_raw_data.signal[detector_number], current_raw_data.preprocess.noise.white_noise[detector_number]
